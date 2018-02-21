32d7b665436b4127f32e07f9435ea5a735469504
robin <engel.robin@gmx.de>
2018-02-21 19:13:06 +0100

    more tests for move armies

--- game/move_armies_test.go
+++ game/move_armies_test.go
@@ -32,4 +32,46 @@ func TestGame_MoveArmies(t *testing.T) {
 			t.Errorf("expected NationB armies to increase to 10 but was %d", nationB.armies)
 		}
 	})
+
+	t.Run("not same occupants", func(t *testing.T) {
+		player1 := Player{name: PlayerName("Player1")}
+		player2 := Player{name: PlayerName("Player1")}
+
+		nationA := Nation{name: NationName("NationA"), occupant: &player1, armies: ArmyCount(10)}
+		nationB := Nation{name: NationName("NationB"), occupant: &player2, armies: ArmyCount(5)}
+
+		game := Game{
+			activePlayer: &player1,
+			world: &World{
+				nations: []*Nation{&nationA, &nationB},
+			},
+		}
+
+		err := game.MoveArmies("NationA", "NationB", ArmyCount(5))
+
+		if err == nil {
+			t.Errorf("expected error")
+		}
+	})
+
+
+	t.Run("not enough armies", func(t *testing.T) {
+		player := Player{name: PlayerName("Player1")}
+
+		nationA := Nation{name: NationName("NationA"), occupant: &player, armies: ArmyCount(10)}
+		nationB := Nation{name: NationName("NationB"), occupant: &player, armies: ArmyCount(5)}
+
+		game := Game{
+			activePlayer: &player,
+			world: &World{
+				nations: []*Nation{&nationA, &nationB},
+			},
+		}
+
+		err := game.MoveArmies("NationA", "NationB", ArmyCount(15))
+
+		if err == nil {
+			t.Errorf("expected error")
+		}
+	})
 }
